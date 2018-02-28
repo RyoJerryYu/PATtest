90129d5c03481e1883800c3649f673ce85e9dad2
RyoJerryYu <tzl19961216@gmail.com>
2018-02-28 16:34:32 +0800

    huge change: use thelist[100002].

--- pro1015/pro1015/main.cpp
+++ pro1015/pro1015/main.cpp
@@ -4,37 +4,31 @@
 #include <stdio.h>
 using namespace std;
 struct listnode {
-	int address, data, next;
+	int data, next;
 };
-int find(const vector<listnode>& thelist, int address) {
-	int size = thelist.size();
-	for (int i = 0; i<size; i++) {
-		if (thelist[i].address == address) { return i; }
-	}
-	return -1;
-}
 int main() {
 	int first, n, k;
+	listnode thelist[100002];
 	cin >> first >> n >> k;
-	vector<listnode> thelist(n);
+	int addri;
 	for (int i = 0; i<n; i++) {
-		cin >> thelist[i].address >> thelist[i].data >> thelist[i].next;
+		cin >> addri;
+		cin >> thelist[addri].data >> thelist[addri].next;
 	}
 	int count = 0;
-	vector<int> addr(n, -1);
-	addr[0] = find(thelist, first);
+	vector<int> addr(n + 1, -1);
+	addr[0] = first;
 	while (addr[count] != -1) {
 		count++;
-		addr[count] = find(thelist, thelist[addr[count - 1]].next);
-		if (thelist[addr[count]].next == -1) { count++; break; }
+		addr[count] = thelist[addr[count - 1]].next;
 	}
-	for (int i = 0; i + k<count; i += k) {
+	for (int i = 0; i + k<=count; i += k) {
 		reverse(addr.begin() + i, addr.begin() + i + k);
 	}
 	for (int i = 0; i<count - 1; i++) {
-		thelist[addr[i]].next = thelist[addr[i + 1]].address;
-		printf("%05d %d %05d\n", thelist[addr[i]].address, thelist[addr[i]].data, thelist[addr[i]].next);
+		thelist[addr[i]].next = addr[i + 1];
+		printf("%05d %d %05d\n", addr[i], thelist[addr[i]].data, thelist[addr[i]].next);
 	}
 	thelist[addr[count - 1]].next = -1;
-	printf("%05d %d -1", thelist[addr[count - 1]].address, thelist[addr[count - 1]].data);
+	printf("%05d %d -1", addr[count - 1], thelist[addr[count - 1]].data);
 }
