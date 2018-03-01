dc1ab0fdd25cf21d91ef50fad7986deb17ae88fe
RyoJerryYu <tzl19961216@gmail.com>
2018-03-01 23:42:19 +0800

    used upper_bound and change max to long long.

--- pro1020/pro1020/main.cpp
+++ pro1020/pro1020/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+/*
 int search(const vector<long> & num, const long & value) {
 	int lo = 0, hi = num.size();
 	while (hi != lo) {
@@ -10,7 +11,7 @@ int search(const vector<long> & num, const long & value) {
 		else lo = mi + 1;
 	}
 	return lo - 1;
-}
+}*/
 int main() {
 	int N;
 	long p;
@@ -20,8 +21,9 @@ int main() {
 	sort(num.begin(), num.end());
 	int q = 1;//p是正整数，至少有一个
 	for (int i = 0; i < N; i++) {
-		long max = num[i] * p;
-		int maxi = search(num, max) + 1;//指向第一个大于max的位置
+		long long max = (long long)num[i] * p;
+		//int maxi = search(num, max) + 1;//指向第一个大于max的位置
+		int maxi = upper_bound(num.begin()+i, num.end(), max)-num.begin();
 		if (maxi - i > q)q = maxi - i;//使得maxi-i为完美序列中数的个数
 	}
 	cout << q;
