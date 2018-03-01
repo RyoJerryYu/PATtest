eae065bef0c3050ce28b966fb838be7a03669919
RyoJerryYu <tzl19961216@gmail.com>
2018-03-01 23:11:00 +0800

    add pro1020.

---
+++ pro1020/pro1020/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int search(const vector<long> & num, const long & value) {
+	int lo = 0, hi = num.size();
+	while (hi != lo) {
+		int mi = (lo + hi) >> 1;
+		if (value < num[mi]) hi = mi;
+		else lo = mi + 1;
+	}
+	return lo - 1;
+}
+int main() {
+	int N;
+	long p;
+	cin >> N >> p;
+	vector<long> num(N);
+	for (int i = 0; i < N; i++) cin >> num[i];
+	sort(num.begin(), num.end());
+	int q = 0;
+	for (int i = 0; i < N; i++) {
+		long max = num[i] * p;
+		int maxi = search(num, max) + 1;//指向第一个大于max的位置
+		if (maxi - i > q)q = maxi - i;//使得maxi-i为完美序列中数的个数
+	}
+	cout << q;
+}
