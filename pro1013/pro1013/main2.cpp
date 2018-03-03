356ef69d44fcdb4f2b16080af39dc2f1bd5623d8
RyoJerryYu <tzl19961216@gmail.com>
2018-03-03 16:47:31 +0800

    reduce an if.

--- pro1013/pro1013/main2.cpp
+++ pro1013/pro1013/main2.cpp
@@ -3,13 +3,11 @@ using namespace std;
 int main() {
 	int a[10] = { 0 }, b = 0;
 	for (int i = 0; i < 10; i++)cin >> a[i];
-	if (a[0] != 0) {
-		for (int i = 1; i < 10; i++) {
-			if (a[i] != 0) {
-				a[i]--;
-				cout << i;
-				break;
-			}
+	for (int i = 1; i < 10; i++) {
+		if (a[i] != 0) {
+			a[i]--;
+			cout << i;
+			break;
 		}
 	}
 	for (int i = 0; i < 10; i++) {
