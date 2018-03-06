ccc891dea81ca99fddce22d447af1979b5653c48
RyoJerryYu <tzl19961216@gmail.com>
2018-03-06 19:52:48 +0800

    update with using STL.

--- pro1020/pro1020/main.cpp
+++ pro1020/pro1020/main.cpp
@@ -2,29 +2,18 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-/*
-int search(const vector<long> & num, const long & value) {
-	int lo = 0, hi = num.size();
-	while (hi != lo) {
-		int mi = (lo + hi) >> 1;
-		if (value < num[mi]) hi = mi;
-		else lo = mi + 1;
-	}
-	return lo - 1;
-}*/
 int main() {
 	int N;
 	long p;
 	cin >> N >> p;
 	vector<long> num(N);
-	for (int i = 0; i < N; i++) cin >> num[i];
+	for (long& x:num) cin >> x;
 	sort(num.begin(), num.end());
 	int q = 1;//p是正整数，至少有一个
-	for (int i = 0; i < N; i++) {
-		long long max = (long long)num[i] * p;
-		//int maxi = search(num, max) + 1;//指向第一个大于max的位置
-		int maxi = upper_bound(num.begin()+i, num.end(), max)-num.begin();//从i开始查找而不是从0开始
-		if (maxi - i > q)q = maxi - i;//使得maxi-i为完美序列中数的个数
+	for (auto i = num.begin(); i != num.end(); i++) {
+		long long max = (long long)*i * p;
+		auto maxi = upper_bound(i, num.end(), max);//从i开始查找而不是从0开始
+		if (maxi - i > q)q = maxi - i;//maxi-i为完美序列中元素的个数
 	}
 	cout << q;
 }
