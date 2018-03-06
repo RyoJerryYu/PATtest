c65cbb58a14db0bd18daad6f566761f5a9eef480
RyoJerryYu <tzl19961216@gmail.com>
2018-03-06 21:44:08 +0800

    add pro1025 with bug.

--- pro1025/pro1025/main.cpp
+++ pro1025/pro1025/main.cpp
@@ -2,10 +2,11 @@
 #include <vector>
 #include <iterator>
 #include <string>
+#include <algorithm>
 using namespace std;
 bool isInsSort(const vector<int>&, const vector<int>&);
-vector<int>&& onceInsSort(const vector<int>&);
-vector<int>&& onceMerSort(const vector<int>&);
+vector<int> onceInsSort(const vector<int>&);
+vector<int> onceMerSort(const vector<int>&);
 int main() {
 	int N;
 	cin >> N;
@@ -13,7 +14,7 @@ int main() {
 	for (int& x : oriseq) cin >> x;
 	for (int& x : midseq) cin >> x;
 	bool typ = isInsSort(oriseq, midseq);
-	cout << (typ ? "Insert Sort" : "Merge Sort") << endl;
+	cout << (typ ? "Insertion Sort" : "Merge Sort") << endl;
 	if (typ) finseq = onceInsSort(midseq);
 	else finseq = onceMerSort(midseq);
 	for (int& x : finseq) cout << x << " ";
@@ -23,9 +24,40 @@ int main() {
 bool isInsSort(const vector<int>& oriseq, const vector<int>& midseq) {
 	auto orip = oriseq.end(), midp = midseq.end();
 	while (*(--orip) == *(--midp));
-	while (orip != oriseq.begin()) {
-		if (*orip < *(orip - 1))return false;
-		orip--;
+	while (midp != midseq.begin()) {
+		if (*midp < *(midp - 1))return false;
+		midp--;
 	}
 	return true;
+}
+vector<int> onceInsSort(const vector<int>& midseq) {
+	auto midp = midseq.begin();
+	vector<int> res;
+	while (*midp <= *(midp + 1))res.push_back(*(midp++));//res前缀部分有序
+	res.push_back(*(midp++));
+	res.insert(upper_bound(res.begin(), res.end(), *midp), *midp);//将前缀后第一个元素有序地插入前缀
+	midp++;
+	const auto mide = midseq.end();
+	while (midp != mide)res.push_back(*(midp++));//后缀部分原样插入
+	return res;
+}
+vector<int> onceMerSort(const vector<int>& midseq) {
+	vector<int> res;
+	auto midp = midseq.begin(), sqend = midseq.end() - 1;
+	back_insert_iterator<vector<int> > resbak(res);
+	int step = 1, shortest = midseq.size();//shortest即为已合并的长度
+	for (midp = midseq.begin(); midp < sqend; midp++) {//`midp<sqend` means `midp+1<end`
+		if (*midp <= *(midp + 1)) step++;
+		else {
+			if (step < shortest) shortest = step;//shortest不包括最后一step
+			step = 1;
+		}
+	}
+	step = shortest << 1;//下一轮迭代
+	sqend = midseq.end() - step;//then `midp<sqend` means `midp+step<end`
+	for (midp = midseq.begin(); midp < sqend; midp += step) {
+		merge(midp, midp + shortest, midp + shortest, midp + step, resbak);//不合并最后一step
+	}
+	if (midp + shortest < midseq.end()) merge(midp, midp + shortest, midp + shortest, midseq.end(), resbak);//合并最后一step
+	return res;
 }
