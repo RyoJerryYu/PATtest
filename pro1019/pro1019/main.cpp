301e71ad4857ec2d3b42970cb70044224a900fa1
RyoJerryYu <tzl19961216@gmail.com>
2018-03-06 18:36:12 +0800

    included algorithm, find another bug.

--- pro1019/pro1019/main.cpp
+++ pro1019/pro1019/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 int main() {
 	string oris, ress, difs = "";
 	cin >> oris >> ress;
 	auto i = oris.begin(), j = ress.begin();
-	const auto orie = oris.end(), rese = ress.end();
-	while (i != orie && j != rese) {
+	const auto rese = ress.end();
+	while (j != rese) {
 		if (*i == *j)i++, j++;
 		else if ('a' <= *i&&*i <= 'z' && (find(difs.begin(), difs.end(), *i - 'a' + 'A')) != difs.end())i++;
 		else if (find(difs.begin(), difs.end(), *i) != difs.end())i++;
