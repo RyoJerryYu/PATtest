a0ee1c586c91dbb1fdbef3c774e321e106c91a43
RyoJerryYu <tzl19961216@gmail.com>
2018-03-08 12:45:02 +0800

    add pro1022.

---
+++ pro1022/pro1022/pro1022.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+struct stu1022 {
+	int No = 0, Mark = 0;
+	bool operator<(stu1022 other) {
+		return Mark < other.Mark;
+	}
+};
+int main() {
+	int N;
+	cin >> N;
+	vector<stu1022> a(N);
+	for (stu1022& x : a) {
+		cin >> x.No >> x.Mark;
+	}
+	sort(a.begin(), a.end());
+	reverse(a.begin(), a.end());
+	cout << a[0].No << " " << a[0].Mark << endl;
+	return 0;
+}
