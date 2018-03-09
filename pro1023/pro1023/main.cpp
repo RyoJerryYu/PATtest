89d75bcae317911da005927999c16bb43518dcd3
RyoJerryYu <tzl19961216@gmail.com>
2018-03-09 21:47:05 +0800

    add pro1023.

---
+++ pro1023/pro1023/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <set>
+#include <string>
+#include <algorithm>
+using namespace std;
+const string upchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int main() {
+	string brostr, brokey="", instr, outstr;
+	cin >> brostr >> instr;
+	for (const char& x : brostr) {
+		if (x == '+')brokey += upchar;
+		else {
+			brokey += (char)tolower(x);
+			brokey += (char)toupper(x);
+		}
+	}
+	sort(brokey.begin(), brokey.end());
+	brokey.erase(unique(brokey.begin(), brokey.end()), brokey.end());
+	for (const char& x : instr) {
+		if (!binary_search(brokey.begin(), brokey.end(), x))outstr += x;
+	}
+	cout << outstr << endl;
+}
