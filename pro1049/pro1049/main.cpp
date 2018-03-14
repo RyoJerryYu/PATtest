110e56193c1aae6f4b26df3eacb55dadb027d015
RyoJerryYu <tzl19961216@gmail.com>
2018-03-14 21:46:44 +0800

    fixed pro1049.

--- pro1049/pro1049/main.cpp
+++ pro1049/pro1049/main.cpp
@@ -9,7 +9,9 @@ int main() {
 	vector<double>group(n);
 	for (int i = 0; i < n; i++)cin >> group[i];
 	for (int i = 0; i < n; i++) {
-		sum += (n - i)*(i + 1)*group[i];
-	}
+		sum += group[i]*(n - i)*(i + 1);
+	}//与(n-i)*(i+1)*group[i]不相同！
+	//前者乘出double再与int相乘出double
+	//后者先乘出int，会数据溢出。
 	printf("%.2f", sum);
 }
